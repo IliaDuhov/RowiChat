0ed13f9e2888316542cc0c46610bf6b0d839c9ab
IliaDuhov <dukhov03@mail.ru>
2023-05-25 23:08:31 +0400

    commit

---
+++ client/src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,40 @@
+package com.example.websocketdemo.controller;
+
+import com.example.websocketdemo.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.net.http.WebSocket;
+
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+    @GetMapping(value = "/support")
+    //@SendTo("/topic/public")
+    public String admin() throws Exception {
+
+        return "support";
+    }
+
+    }
